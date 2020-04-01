64f69b40c3876e7f85ac3d1d36e24a99d5cd3bf8
Hyun Woo Lim <36051245+HungryLim@users.noreply.github.com>
2020-03-31 21:07:30 -0500

    Add files via upload

---
+++ searching_donors.R
@@ -0,0 +1,44 @@
+setwd("C:/Users/wooki/Desktop/bonica")
+setwd("C:/Users/Dell/Desktop/Bonica")
+
+
+#want<-read.csv("bod_fortune_500_DIME_cont_records.csv",header=TRUE)
+name<-read.csv("corp_name_loop3.csv",header=TRUE)
+names<-name$most.recent.contributor.name
+names<-unique(names)
+
+transactFile <- 'dime_contributors_1979_2014.csv'
+
+readLines(transactFile, n=1)
+higgs_colnames <- c("bonica.cid","contributor.type","num.records","num.distinct","most.recent.contributor.name","most.recent.contributor.address","most.recent.contributor.city","most.recent.contributor.zipcode","most.recent.contributor.state","most.recent.contributor.occupation","most.recent.contributor.employer","most.recent.transaction.id","contributor.gender","is_corp","contributor.cfscore","is.projected","first_cycle_active","last_cycle_active","amount_1980","amount_1982","amount_1984","amount_1986","amount_1988","amount_1990","amount_1992","amount_1994","amount_1996","amount_1998","amount_2000","amount_2002","amount_2004","amount_2006","amount_2008","amount_2010","amount_2012","amount_2014")
+###
+index <- 0
+chunkSize <- 100000
+con <- file(description=transactFile,open="r")   
+dataChunk <- read.table(con, nrows=chunkSize, header=T, fill=TRUE, sep=",", col.names=higgs_colnames)
+datalist = list()
+#r.cfscore  <- NA
+
+repeat {
+  index <- index + 1
+  print(paste('Processing rows:', index * chunkSize))
+  #datalist[[index]]<- dataChunk$candidate.cfscore[match(want$dime.rid, dataChunk$bonica.rid)]
+  datalist[[index]]<- dataChunk[dataChunk$most.recent.contributor.name %in% names, ]
+  
+  if (nrow(dataChunk) != chunkSize){
+    print('Processed all files!')
+    break}
+  
+  dataChunk <- read.table(con, nrows=chunkSize, skip=0, header=FALSE, fill = TRUE, sep=",", col.names=higgs_colnames)
+  
+  if (index > 165) break #307
+  
+}
+close(con)
+
+nrow(datalist)
+big_data = do.call(rbind, datalist)
+nrow(big_data)
+write.csv(big_data, file = "big_dataff33.csv")
+
+#sum(is.na(r.cfscore))
